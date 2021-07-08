f0b48ebbc4d6e2c8fafdf9fee3b7d79cfb6d1dd4
GitBolt <stingbolt1@gmail.com>
2021-07-08 21:36:35 +0530

    🚀 Added chatbot here

--- axiolapi/main.py
+++ axiolapi/main.py
@@ -1,8 +1,15 @@
+import torch
+import json
+import random
+from chatbot.model import NeuralNet
+from chatbot.utils import bag_of_words, tokenize, stemmer
 from fastapi import FastAPI
 from visuals.bargraph import BarGraph
 from visuals.piechart import PieChart
 
 app = FastAPI()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 @app.get('/')
@@ -29,3 +36,54 @@ def index(serverid: int):
     Data = PieChart(serverid)
         
     return {"message": Data}
+
+@app.post("/ai/chatbot", status_code=200)
+async def chatbot(content:str):
+
+    with open('chatbot/intents.json', 'r') as json_data:
+        intents = json.load(json_data)
+
+    FILE = "chatbot/data.pth"
+    data = torch.load(FILE, map_location='cpu')
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
+    sentence = content.strip(f"<@!{843484459113775114}>") #Removing the bot ping
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.85:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                response = random.choice(intent["responses"])
+                return {"response": response, "tag": tag}
+
+    else:
+        return {"response": random.choice([
+                        "What?",
+                        "Sorry what?",
+                        "?",
+                        "Didn't understand",
+                        "Huh",
+                        ":face_with_raised_eyebrow:",
+                        "what",
+                        "Can you say that differently?",
+                        "Hmm?"
+                    ]), "tag": "unknown"}
